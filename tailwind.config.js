c494eb31ec424944d5b5474a1ed36593559bc2f3
Maxime <maxime.britto@edu.devinci.fr>
2023-11-20 16:51:32 +0100

    2eme projets

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,38 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
-  },
+    extend: {
+      colors: {
+        "primary": "#1C85E8",
+        "secondary": {
+          dark: "#020202",
+          grey: "#B8B8B8"
+        }
+      },
+      fontFamily: {
+        body: ["Montserrat"]
+      },
+      fontSize: {
+        "4.5xl": "2.5rem"
+      },
+      spacing: {
+        128: "32rem",
+        144: "36rem"
+      },
+    margin: {
+      "mediumWrapper": "8rem",
+      
+    },
+    padding: {
+      "mediumWrapper": "8rem",
+      
+    },
+
+    },
+    },
+    
+  
   plugins: [],
 }
 
